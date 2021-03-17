152b8c1656228cfe001afe5479ea4bde7ee77ce8
Гусев Виталий Евгеньевич <Vitaly.Gusev@norbit.ru>
2021-03-17 11:59:10 +0300

    nir 2

--- Vichislitelnie_metodi/Vichislitelnie_metodi_26.py
+++ Vichislitelnie_metodi/Vichislitelnie_metodi_26.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 a = 0.0
 b = 2.0
 #h = 0.2
-h = (b - a) / 40
+h = (b - a) / 10
 eps = 10 ** (-6)
 
 f = []
@@ -52,27 +52,37 @@ def l(x, k, func):
             sum += product_series_numerator(x, k, j, func) / product_series_denominator(func[k][0], k, func)
     return sum
 
-def result2(func):
+def result2(func, x):
     res = []
-    for i in range(len(func)):
+    for i in x:
         sum = 0
         row = []
         for k in range(len(func)):
-            sum += func[k][1] * l(func[i][0], k, func)
-        row.append(func[i][0])
+            sum += func[k][1] * l(i, k, func)
+        row.append(i)
         row.append(sum)
-        help = (2 / np.sqrt(np.pi)) * np.exp((-1) * func[i][0] ** 2)
+        help = (2 / np.sqrt(np.pi)) * np.exp((-1) * i ** 2)
         row.append(help)
+        row.append(sum - help)
         res.append(row)
     return res
 
+x = []
+step = 0
+for i in range(200):
+    res = step
+    x.append(res)
+    step += 0.01
 
-res2 = result2(f)
+res2 = result2(f, x)
+for i in res2:
+    print(i)
 plt.title(str(h))
 row0 = []
 row1 = []
 for i in res2:
     row0.append(i[0])
-    row1.append(i[1])
+    row1.append(i[1] - i[2])
 plt.plot(row0, row1)
 plt.show()
+#проверить между узлами
