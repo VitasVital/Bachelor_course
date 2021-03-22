d3b3a69a644ac3f3292d7996b3ce387d1442ba4b
Гусев Виталий Евгеньевич <Vitaly.Gusev@norbit.ru>
2021-03-22 11:34:16 +0300

    2 nir

--- NIR2/Решение.py
+++ NIR2/Решение.py
@@ -1,13 +1,27 @@
 import numpy as np
 from scipy.optimize import linprog
 
-a_l = [[55, 69, 77, 58, 93],
-       [87, 99, 60, 85, 76],
-       [94, 56, 86, 55, 64]]
-b_l = [2211, 4836, 3712]
-c = [74, 99, 91, 87, 95]
-c_new = np.dot(c, -1)
+f1 = open('a_l_read.txt', 'r')
+f2 = open('b_l_read.txt', 'r')
+f3 = open('c_read.txt', 'r')
+a_l1 = []
+a_l1_copy = [line.split() for line in f1]
+b_l1 = f2.readline().split()
+c1 = f3.readline().split()
 
-res_lin = linprog(c_new, a_l, b_l)
-print(res_lin)
-print(np.dot(res_lin.fun, -1))
+for i in a_l1_copy:
+    array = []
+    for j in i:
+        array.append(float(j))
+    a_l1.append(array)
+b_l1 = [float(elem) for elem in b_l1]
+c1 = [float(elem) for elem in c1]
+c_new1 = np.dot(c1, -1)
+
+res_lin1 = linprog(c_new1, a_l1, b_l1)
+print(res_lin1)
+print(np.dot(res_lin1.fun, -1))
+
+f1.close()
+f2.close()
+f3.close()
