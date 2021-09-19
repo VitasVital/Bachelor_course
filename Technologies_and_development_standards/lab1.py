d1063d9323f102b76c284c86008e2f47e1e554d7
Гусев Виталий Евгеньевич <Vitaly.Gusev@norbit.ru>
2021-09-19 13:42:12 +0300

    delete all string

--- Technologies_and_development_standards/lab1.py
+++ Technologies_and_development_standards/lab1.py
@@ -7,10 +7,31 @@ import pandas as pd
 operators = ['=', '[', '(']
 f = open('program.txt', 'r')
 
-l = [line.strip() for line in f]
+array = [row.strip() for row in f]
+for i in array:
+    if(i == ''):
+        array.remove(i) #удалить пробелы
 
-for i in l:
+new_array = []
+for i in array:
+    new_array.append(list(i)) #разбить строки на символы
+
+for i in range(len(new_array) - 1): #удаление всех string в строках
+    start_symbol = False
+    for j in range(len(new_array[i]) - 1):
+        if (start_symbol == False and new_array[i][j] == '"'):
+            start_symbol = True
+            new_array[i][j] = False
+        elif (start_symbol == True and new_array[i][j] == '"'):
+            start_symbol = False
+            new_array[i][j] = False
+        elif (start_symbol == True):
+            new_array[i][j] = False
+    while(False in new_array[i]):
+        new_array[i].remove(False)
+
+
+for i in new_array:
     print(i)
 
-for i in operators:
-    print(i)
+f.close()
