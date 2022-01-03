711bee69b1b03ee75f36f195d107d509bdb9b8fb
Гусев Виталий Евгеньевич <Vitaly.Gusev@norbit.ru>
2022-01-03 13:06:24 +0300

    Games theory end

--- Games_theory/Games_theory_3.py
+++ Games_theory/Games_theory_3.py
@@ -22,11 +22,39 @@ def get_all_K_json(N):
     list_to_dictionary = []
 
     for i in range(len(list_k)):
-        data_set = {"id": i, "combinations": list_k[i]}
+        data_set = {"id": i, "combinations": list_k[i], "value": 0}
         list_to_dictionary.append(data_set)
 
     json_object = list_to_dictionary
     return json_object
+
+def get_all_v_K_json(N, V_k):
+    list_k = get_all_K(N)
+    for i in range(len(list_k)):
+        str1 = ', '.join(str(e) for e in list_k[i])
+        list_k[i] = str1
+
+    list_to_dictionary = []
+
+    for i in range(len(list_k)):
+        data_set = {"id": i, "combinations": list_k[i], "value": V_k[i]}
+        list_to_dictionary.append(data_set)
+
+    json_object = list_to_dictionary
+    return json_object
+
+def get_all_random_v_K_json(N):
+    list_k = get_all_K(N)
+    for i in range(len(list_k)):
+        str1 = ', '.join(str(e) for e in list_k[i])
+        list_k[i] = str1
+
+    list_v_k = str(rn.randrange(100, 600))
+
+    for i in range(1, len(list_k)):
+        list_v_k += (' ' + str(rn.randrange(100, 600)))
+
+    return list_v_k
 
 def start_function():
     N = 3
@@ -71,4 +99,52 @@ def start_function():
     print(sum_row / len(res_row))
     return sum_row
 
-print(get_all_K_json(3))
+
+
+def find_Sheply(N, V_k):
+    k = get_all_K(N)
+
+    res_array = []
+
+    for item in it.permutations(range(N), N): #все комбинации последовательностей формирования общей коалиции
+        res_array.append([i + 1 for i in list(item)])
+
+    res_row = []
+    for row in res_array:
+        row_add = [0 for i in range(N)] #инициализируем строку для вектора шепли
+        row_add[row[0] - 1] = V_k[row[0]]
+        last_element_v_k = V_k[row[0]]
+        for i in range(1, len(row)):
+            for j in range(len(k)):
+                if sorted(row[:i + 1]) == sorted(k[j]):
+                    index_row = row[:i + 1]
+                    row_add[index_row[len(index_row) - 1] - 1] = V_k[j] - last_element_v_k
+                    last_element_v_k = V_k[j]
+        res_row.append(row_add)
+
+    res_row = np.array(res_row)
+
+    sum_row = res_row.T @ np.ones(len(res_row))
+
+    list_to_dictionary = []
+
+
+    new_res_array = []
+    for element in res_array:
+        new_res_array.append(str(element))
+
+    new_res_row = []
+    for element in res_row:
+        new_res_row.append(str(element))
+
+    for i in range(len(res_array)):
+        data_set = {"id": i, "contributions": new_res_array[i].replace(", ", " -> "), "player_sequence": new_res_row[i]}
+        list_to_dictionary.append(data_set)
+
+    data_set = {"id": len(res_array), "contributions": 'Вектор Шепли', "player_sequence": str(sum_row / len(res_array))}
+    list_to_dictionary.append(data_set)
+
+    return list_to_dictionary
+
+
+print(get_all_random_v_K_json(3))
--- Games_theory/web_app.py
+++ Games_theory/web_app.py
@@ -2,45 +2,36 @@
 from flask import Flask, jsonify, request, Response, abort
 from flask_cors import CORS, cross_origin
 import Games_theory_3 as gm3
+import random as rn
 
 app = Flask(__name__)
 CORS(app)
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
-        'done': False
-    }
-]
-
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
-def get_tasks():
-    return jsonify({'tasks': tasks})
-
-@app.route('/todo/api/v1.0/tasks', methods=['POST'])
-def create_task():
-    if not request.json or not 'title' in request.json:
-        abort(400)
-    task = {
-        'id': tasks[-1]['id'] + 1,
-        'title': request.json['title'],
-        'description': request.json.get('description', ""),
-        'done': False
-    }
-    tasks.append(task)
-    return jsonify({'task': task}), 201
-
 @app.route('/todo/api/v1.0/get_k', methods=['POST'])
 def get_k():
     result = gm3.get_all_K_json(int(request.json['N']))
+    return jsonify(result)
+
+@app.route('/todo/api/v1.0/get_v_k', methods=['POST'])
+def get_v_k():
+    V_k = request.json['V_k']
+    N = int(request.json['N'])
+    result = gm3.get_all_v_K_json(N, V_k.split())
+    return jsonify(result)
+
+@app.route('/todo/api/v1.0/get_random_v_k', methods=['POST'])
+def get_random_v_k():
+    N = int(request.json['N'])
+    result = gm3.get_all_random_v_K_json(N)
+    return jsonify(result)
+
+@app.route('/todo/api/v1.0/find_sheply', methods=['POST'])
+def find_sheply():
+    N = int(request.json['N'])
+    V_k = request.json['V_k']
+    V_k = V_k.split()
+    V_k_int = [int(x) for x in V_k]
+    result = gm3.find_Sheply(N, V_k_int)
     return jsonify(result)
 
 if __name__ == '__main__':
