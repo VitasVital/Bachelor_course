7dae8b71a92b6d6ae9810e2a228d7acb57c0928d
Гусев Виталий Евгеньевич <Vitaly.Gusev@norbit.ru>
2023-03-04 17:03:25 +0300

    ИБ задание 1

--- Maga2Sem/Information security/Gusev V.E. 09-235 Информационная безопасность.py
+++ Maga2Sem/Information security/Gusev V.E. 09-235 Информационная безопасность.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def step1(input_text):
     bit_text = ''
     for char in input_text:
@@ -18,18 +20,41 @@ def step3(binary_text, r):
         B.append(B_res)
     return B
 
-def step4(B):
+def step4(B1):
+    n = 3
+    m = 3
+    k = 5
+    A = np.arange(45).reshape(3, 3, 5) # Задаем матрицу A размером 3 x 3 х 5.
+    ii = 0
+    for i in range(0, n):  # Заполняет ее элементами вектора B1
+        for j in range(0, m):
+            for h in range(0, k):
+                A[i][j][h] = B1[ii]
+                ii += 1
+    return A
 
-    return
+def step5(A):
+    V = []
+    for k in range(0, 5):
+        V_ind = ''
+        for n in range(0, 3):
+            for m in range(0, 3):
+                V_ind += str(A[n][m][k])
+        V.append(V_ind)
+    return V
 
 def task1():
     input_text = input('Введите текст')
     bit_text = step1(input_text)
-    print('Битовая последовательность:\n', bit_text)
+    print('Битовая последовательность:\n', bit_text, '\n')
     r = 25
     binary_text = step2(bit_text, r)
-    print('Бинарная последовательность:\n', binary_text)
+    print('Бинарная последовательность:\n', binary_text, '\n')
     B = step3(binary_text, r)
-    step4(B)
+    for B1 in B:
+        A = step4(B1)
+        V = step5(A)
+        print('Блок B\n', B1)
+        print('     Бинарные вектора V\n', '    ', V, '\n')
 
 task1()
