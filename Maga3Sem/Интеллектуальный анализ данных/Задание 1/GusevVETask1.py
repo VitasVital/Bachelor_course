cee26ed0b72d7c84d52ae90426e53895cbe43a33
Vitaly.Gusev <Vitaly.Gusev@omnichannel.ru>
2025-02-12 09:51:47 +0300

    Задание 1

--- Maga3Sem/Интеллектуальный анализ данных/Задание 1/GusevVETask1.py
+++ Maga3Sem/Интеллектуальный анализ данных/Задание 1/GusevVETask1.py
@@ -1,5 +1,9 @@
 import numpy as np
+import pandas as pd
 
+excel_path = 'C:/Users/vitaly.gusev/Desktop/Универ/Bachelor_course/Maga3Sem/Интеллектуальный анализ данных/Задание 1/GusevVE_Task1.xlsx'
+
+# функция вычисления Манхэттенского расстояния
 def ManhattanDistance(input_points):
     result_function = np.zeros((len(input_points), len(input_points)))
     for i in range(0, len(input_points)):
@@ -7,6 +11,7 @@ def ManhattanDistance(input_points):
             result_function[i, j] = np.abs(input_points[j, 0] - input_points[i, 0]) + np.abs(input_points[j, 1] - input_points[i, 1])
     return result_function
 
+# функция вычисления расстояния Хэмминга
 def HamingDistance(input_points):
     result_function = np.zeros((len(input_points), len(input_points)))
     for i in range(0, len(input_points)):
@@ -14,6 +19,7 @@ def HamingDistance(input_points):
             result_function[i, j] = np.sign(input_points[j, 0] - input_points[i, 0]) + np.sign(input_points[j, 1] - input_points[i, 1])
     return result_function
 
+# функция вычисления Евклидова расстояния
 def EuclideanDistance(input_points):
     result_function = np.zeros((len(input_points), len(input_points)))
     for i in range(0, len(input_points)):
@@ -21,13 +27,22 @@ def EuclideanDistance(input_points):
             result_function[i, j] = np.sqrt((input_points[j, 0] - input_points[i, 0])**2 + (input_points[j, 1] - input_points[i, 1])**2)
     return result_function
 
-points = np.array([[2, 2, 4, 4, 5, 5, 7, 7],
-             [1, 5, 3, 6, 4, 5, 2, 5]]).T
+# считывание массива наблюдений из excel
+df_orders = pd.read_excel(excel_path, index_col=None, header=None)
+points = np.array(df_orders)
+print(np.array(points))
 
+# points = np.array([[2, 2, 4, 4, 5, 5, 7, 7],
+#              [1, 5, 3, 6, 4, 5, 2, 5]]).T
+
+# вычисление Манхэттенского расстояния
 manhattanDistance = ManhattanDistance(points)
+# вычисление расстояния Хэмминга
 hamingDistance = HamingDistance(points)
+# вычисление Евклидова расстояния
 euclideanDistance = EuclideanDistance(points)
 
+# вывод результатов расстояний
 print('Манхэттенское расстояние\n', manhattanDistance)
 print('Расстояние Хэмминга\n', hamingDistance)
 print('Евклидово расстояние\n', euclideanDistance)
