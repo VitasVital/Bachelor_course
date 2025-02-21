613394890353f042f3d16743060c4fc178a19b85
Vitaly.Gusev <Vitaly.Gusev@omnichannel.ru>
2025-02-21 09:25:30 +0300

    Задание 2

--- Maga3Sem/Интеллектуальный анализ данных/Задание 2/GusevVETask2.py
+++ Maga3Sem/Интеллектуальный анализ данных/Задание 2/GusevVETask2.py
@@ -18,56 +18,6 @@ class HierarchicalAgglomerativeClustering:
                                '_method1_distance' : self._method1_distance,
                                '_method2_distance' : self._method2_distance,
                                }[linkage]
-        
-    # метод односвязной кластеризации
-    @staticmethod
-    def _min_distance(c1, c2):
-        distances = []
-        for i in c1:
-            for j in c2:
-                distances.append(sqeuclidean(i, j))
-        print(distances)
-        return np.min(distances)
-    
-    # метод полносвязной кластеризации
-    @staticmethod
-    def _max_distance(c1, c2):
-        distances = []
-        for i in c1:
-            for j in c2:
-                distances.append(sqeuclidean(i, j))
-        print(distances)
-        return np.max(distances)
-    
-    # метод среднего расстояния
-    @staticmethod
-    def _avg_distance(c1, c2):
-        distances = []
-        for i in c1:
-            for j in c2:
-                distances.append(sqeuclidean(i, j))
-        print(distances)
-        return np.average(distances)
-    
-    # Метод 1. Полусумма минимального и максимального расстояния между объектами из двух кластеров 
-    @staticmethod
-    def _method1_distance(c1, c2):
-        distances = []
-        for i in c1:
-            for j in c2:
-                distances.append(sqeuclidean(i, j))
-        print(distances)
-        return (np.min(distances) + np.max(distances)) / 2
-    
-    # Метод 2. Медианное расстояние между всеми парами объектов из двух кластеров
-    @staticmethod
-    def _method2_distance(c1, c2):
-        distances = []
-        for i in c1:
-            for j in c2:
-                distances.append(sqeuclidean(i, j))
-        print(distances)
-        return np.median(distances)
     
     #метки с минимальным расстоянием, то есть ближайшие кластеры объединяются в новый следующим образом:
     #всем объектам одного кластера присваиваются метки другого, после чего все новые метки,
@@ -80,48 +30,196 @@ class HierarchicalAgglomerativeClustering:
         labels[labels > min_cdist_idxs[1]] -= 1
 
         return labels
+        
+    # метод односвязной кластеризации
+    @staticmethod
+    def _min_distance(self):
+        distance_i = 1
+        distance_j = 0
+        for i in range(1, len(self.clusters) - 1):
+            for j in range(0, i):
+                if self.clusters[i, j] < self.clusters[distance_i, distance_j]:
+                    distance_i = i
+                    distance_j = j
+        distance = self.clusters[distance_i, distance_j]
+        for i in range(0, len(self.clusters)):
+            distance_i_i = self.clusters[distance_i][i] if self.clusters[distance_i][i] != 0 else self.clusters[i][distance_i]
+            distance_j_j = self.clusters[distance_j][i] if self.clusters[distance_j][i] != 0 else self.clusters[i][distance_j]
+            self.clusters[distance_i, i] = min(distance_i_i, distance_j_j)
+            self.clusters[distance_j, i] = min(distance_i_i, distance_j_j)
+            self.clusters[i, distance_i] = min(distance_i_i, distance_j_j)
+            self.clusters[i, distance_j] = min(distance_i_i, distance_j_j)
+        self.clusters_points[distance_i].append(self.clusters_points[distance_j])
+        self.clusters_points.pop(distance_j)
+        self.clusters = np.delete(self.clusters, (distance_j), axis=0) # удаление строки
+        self.clusters = np.delete(self.clusters, (distance_j), axis=1) # удаление колонки
+        self.labels = self._update_labels(self.labels, (distance_i, distance_j))
+        for clusters_point in self.clusters_points:
+            print(clusters_point)
+
+        print("n = len(clusters) = ", len(self.clusters))
+        print("Номера кластеров для точек: ", np.array(self.labels))
+        print("Минимальное рассояние между кластерами равно ", distance)
+        print("И это расстояние между кластерами в D: ", (distance_i, distance_j))
+        print('Матрица Dn расстояний между кластерами\n', self.clusters)
+        
+    
+    # метод полносвязной кластеризации
+    @staticmethod
+    def _max_distance(self):
+        distance_i = 1
+        distance_j = 0
+        for i in range(1, len(self.clusters) - 1):
+            for j in range(0, i):
+                if self.clusters[i, j] < self.clusters[distance_i, distance_j]:
+                    distance_i = i
+                    distance_j = j
+        distance = self.clusters[distance_i, distance_j]
+        for i in range(0, len(self.clusters)):
+            distance_i_i = self.clusters[distance_i][i] if self.clusters[distance_i][i] != 0 else self.clusters[i][distance_i]
+            distance_j_j = self.clusters[distance_j][i] if self.clusters[distance_j][i] != 0 else self.clusters[i][distance_j]
+            self.clusters[distance_i, i] = max(distance_i_i, distance_j_j)
+            self.clusters[distance_j, i] = max(distance_i_i, distance_j_j)
+            self.clusters[i, distance_i] = max(distance_i_i, distance_j_j)
+            self.clusters[i, distance_j] = max(distance_i_i, distance_j_j)
+        self.clusters_points[distance_i].append(self.clusters_points[distance_j])
+        self.clusters_points.pop(distance_j)
+        self.clusters = np.delete(self.clusters, (distance_j), axis=0) # удаление строки
+        self.clusters = np.delete(self.clusters, (distance_j), axis=1) # удаление колонки
+        self.labels = self._update_labels(self.labels, (distance_i, distance_j))
+        for clusters_point in self.clusters_points:
+            print(clusters_point)
+
+        print("n = len(clusters) = ", len(self.clusters))
+        print("Номера кластеров для точек: ", np.array(self.labels))
+        print("Максимальное рассояние между кластерами равно ", distance)
+        print("И это расстояние между кластерами в D: ", (distance_i, distance_j))
+        print('Матрица Dn расстояний между кластерами\n', self.clusters)
+    
+    # метод среднего расстояния
+    @staticmethod
+    def _avg_distance(self):
+        distance_i = 1
+        distance_j = 0
+        for i in range(1, len(self.clusters) - 1):
+            for j in range(0, i):
+                if self.clusters[i, j] < self.clusters[distance_i, distance_j]:
+                    distance_i = i
+                    distance_j = j
+        distance = self.clusters[distance_i, distance_j]
+        for i in range(0, len(self.clusters)):
+            distance_i_i = self.clusters[distance_i][i] if self.clusters[distance_i][i] != 0 else self.clusters[i][distance_i]
+            distance_j_j = self.clusters[distance_j][i] if self.clusters[distance_j][i] != 0 else self.clusters[i][distance_j]
+            clusters_points_i_len = len(self.clusters_points[distance_i])
+            clusters_points_j_len = len(self.clusters_points[distance_j])
+            self.clusters[distance_i, i] = (distance_i_i * clusters_points_i_len + distance_j_j * clusters_points_j_len) / (clusters_points_i_len + clusters_points_j_len)
+            self.clusters[distance_j, i] = (distance_i_i * clusters_points_i_len + distance_j_j * clusters_points_j_len) / (clusters_points_i_len + clusters_points_j_len)
+            self.clusters[i, distance_i] = (distance_i_i * clusters_points_i_len + distance_j_j * clusters_points_j_len) / (clusters_points_i_len + clusters_points_j_len)
+            self.clusters[i, distance_j] = (distance_i_i * clusters_points_i_len + distance_j_j * clusters_points_j_len) / (clusters_points_i_len + clusters_points_j_len)
+        self.clusters_points[distance_i].append(self.clusters_points[distance_j])
+        self.clusters_points.pop(distance_j)
+        self.clusters = np.delete(self.clusters, (distance_j), axis=0) # удаление строки
+        self.clusters = np.delete(self.clusters, (distance_j), axis=1) # удаление колонки
+        self.labels = self._update_labels(self.labels, (distance_i, distance_j))
+        for clusters_point in self.clusters_points:
+            print(clusters_point)
+
+        print("n = len(clusters) = ", len(self.clusters))
+        print("Номера кластеров для точек: ", np.array(self.labels))
+        print("Среднее рассояние между кластерами равно ", distance)
+        print("И это расстояние между кластерами в D: ", (distance_i, distance_j))
+        print('Матрица Dn расстояний между кластерами\n', self.clusters)
+    
+    # Метод 1. Полусумма минимального и максимального расстояния между объектами из двух кластеров 
+    @staticmethod
+    def _method1_distance(self):
+        distance_i = 1
+        distance_j = 0
+        for i in range(1, len(self.clusters) - 1):
+            for j in range(0, i):
+                if self.clusters[i, j] < self.clusters[distance_i, distance_j]:
+                    distance_i = i
+                    distance_j = j
+        distance = self.clusters[distance_i, distance_j]
+        for i in range(0, len(self.clusters)):
+            distance_i_i = self.clusters[distance_i][i] if self.clusters[distance_i][i] != 0 else self.clusters[i][distance_i]
+            distance_j_j = self.clusters[distance_j][i] if self.clusters[distance_j][i] != 0 else self.clusters[i][distance_j]
+            clusters_points_i_len = len(self.clusters_points[distance_i])
+            clusters_points_j_len = len(self.clusters_points[distance_j])
+            self.clusters[distance_i, i] = (distance_i_i * clusters_points_i_len + distance_j_j * clusters_points_j_len) / (clusters_points_i_len + clusters_points_j_len)
+            self.clusters[distance_j, i] = (distance_i_i * clusters_points_i_len + distance_j_j * clusters_points_j_len) / (clusters_points_i_len + clusters_points_j_len)
+            self.clusters[i, distance_i] = (distance_i_i * clusters_points_i_len + distance_j_j * clusters_points_j_len) / (clusters_points_i_len + clusters_points_j_len)
+            self.clusters[i, distance_j] = (distance_i_i * clusters_points_i_len + distance_j_j * clusters_points_j_len) / (clusters_points_i_len + clusters_points_j_len)
+        self.clusters_points[distance_i].append(self.clusters_points[distance_j])
+        self.clusters_points.pop(distance_j)
+        self.clusters = np.delete(self.clusters, (distance_j), axis=0) # удаление строки
+        self.clusters = np.delete(self.clusters, (distance_j), axis=1) # удаление колонки
+        self.labels = self._update_labels(self.labels, (distance_i, distance_j))
+        for clusters_point in self.clusters_points:
+            print(clusters_point)
+
+        print("n = len(clusters) = ", len(self.clusters))
+        print("Номера кластеров для точек: ", np.array(self.labels))
+        print("Среднее рассояние между кластерами равно ", distance)
+        print("И это расстояние между кластерами в D: ", (distance_i, distance_j))
+        print('Матрица Dn расстояний между кластерами\n', self.clusters)
+    
+    # Метод 2. Медианное расстояние между всеми парами объектов из двух кластеров (сделать)
+    @staticmethod
+    def _method2_distance(self):
+        distance_i = 1
+        distance_j = 0
+        for i in range(1, len(self.clusters) - 1):
+            for j in range(0, i):
+                if self.clusters[i, j] < self.clusters[distance_i, distance_j]:
+                    distance_i = i
+                    distance_j = j
+        distance = self.clusters[distance_i, distance_j]
+        for i in range(0, len(self.clusters)):
+            distance_i_i = self.clusters[distance_i][i] if self.clusters[distance_i][i] != 0 else self.clusters[i][distance_i]
+            distance_j_j = self.clusters[distance_j][i] if self.clusters[distance_j][i] != 0 else self.clusters[i][distance_j]
+            clusters_points_i_len = len(self.clusters_points[distance_i])
+            clusters_points_j_len = len(self.clusters_points[distance_j])
+            self.clusters[distance_i, i] = (distance_i_i * clusters_points_i_len + distance_j_j * clusters_points_j_len) / (clusters_points_i_len + clusters_points_j_len)
+            self.clusters[distance_j, i] = (distance_i_i * clusters_points_i_len + distance_j_j * clusters_points_j_len) / (clusters_points_i_len + clusters_points_j_len)
+            self.clusters[i, distance_i] = (distance_i_i * clusters_points_i_len + distance_j_j * clusters_points_j_len) / (clusters_points_i_len + clusters_points_j_len)
+            self.clusters[i, distance_j] = (distance_i_i * clusters_points_i_len + distance_j_j * clusters_points_j_len) / (clusters_points_i_len + clusters_points_j_len)
+        self.clusters_points[distance_i].append(self.clusters_points[distance_j])
+        self.clusters_points.pop(distance_j)
+        self.clusters = np.delete(self.clusters, (distance_j), axis=0) # удаление строки
+        self.clusters = np.delete(self.clusters, (distance_j), axis=1) # удаление колонки
+        self.labels = self._update_labels(self.labels, (distance_i, distance_j))
+        for clusters_point in self.clusters_points:
+            print(clusters_point)
+
+        print("n = len(clusters) = ", len(self.clusters))
+        print("Номера кластеров для точек: ", np.array(self.labels))
+        print("Среднее рассояние между кластерами равно ", distance)
+        print("И это расстояние между кластерами в D: ", (distance_i, distance_j))
+        print('Матрица Dn расстояний между кластерами\n', self.clusters)
+
+
+        # distances = []
+        # for i in c1:
+        #     for j in c2:
+        #         distances.append(sqeuclidean(i, j))
+        # print(distances)
+        # return np.median(distances)
     
     # функция предсказания
     def fit_predict(self, points):
-        clusters = ManhattanDistance(points)
-        labels = np.arange(len(points))
-        print('Манхэттенское расстояние\n', clusters)
+        self.clusters = ManhattanDistance(points)
+        print('Манхэттенское расстояние\n', self.clusters)
+        self.labels = np.arange(len(points))
         
-        clusters_points = [[point] for point in points]
+        self.clusters_points = [[point] for point in points]
         step = 0
-        while len(clusters) > self.n_clusters:
+        while len(self.clusters) > self.n_clusters:
             print("\nШаг ", step)
-            minDistance_i = 1
-            minDistance_j = 0
-            for i in range(1, len(clusters) - 1):
-                for j in range(0, i):
-                    if clusters[i, j] < clusters[minDistance_i, minDistance_j]:
-                        minDistance_i = i
-                        minDistance_j = j
-            minDistance = clusters[minDistance_i, minDistance_j]
-            for i in range(0, len(clusters)):
-                minDistance_i_i = clusters[minDistance_i][i] if clusters[minDistance_i][i] != 0 else clusters[i][minDistance_i]
-                minDistance_j_j = clusters[minDistance_j][i] if clusters[minDistance_j][i] != 0 else clusters[i][minDistance_j]
-                clusters[minDistance_i, i] = min(minDistance_i_i, minDistance_j_j)
-                clusters[minDistance_j, i] = min(minDistance_i_i, minDistance_j_j)
-                clusters[i, minDistance_i] = min(minDistance_i_i, minDistance_j_j)
-                clusters[i, minDistance_j] = min(minDistance_i_i, minDistance_j_j)
-            clusters_points[minDistance_i].append(clusters_points[minDistance_j])
-            clusters_points.pop(minDistance_j)
-            clusters = np.delete(clusters, (minDistance_j), axis=0) # удаление строки
-            clusters = np.delete(clusters, (minDistance_j), axis=1) # удаление колонки
-            labels = self._update_labels(labels, (minDistance_i, minDistance_j))
-            for clusters_point in clusters_points:
-                print(clusters_point)
-
-            print("n = len(clusters) = ", len(clusters))
-            print("Номера кластеров для точек: ", np.array(labels))
-            print("Минимальное рассояние между кластерами равно ", minDistance)
-            print("И это расстояние между кластерами в D: ", (minDistance_i, minDistance_j))
-            print('Матрица Db расстояний между кластерами', clusters)
+            self.calculate_cdist(self)
             step+=1
 
-        return np.array(labels)
+        return np.array(self.labels)
 
 # создание массива наблюдений
 points = np.array([[2, 2, 4, 4, 5, 5, 7, 7],
