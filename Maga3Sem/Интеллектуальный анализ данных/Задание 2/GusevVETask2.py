26b239e8a9380763bc7b7ccbc770e4adb990406b
Vitaly.Gusev <Vitaly.Gusev@omnichannel.ru>
2025-02-22 13:14:50 +0300

    task2

--- Maga3Sem/Интеллектуальный анализ данных/Задание 2/GusevVETask2.py
+++ Maga3Sem/Интеллектуальный анализ данных/Задание 2/GusevVETask2.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.spatial.distance import sqeuclidean
+import copy
 
 # функция вычисления Манхэттенского расстояния
 def ManhattanDistance(input_points):
@@ -18,6 +19,13 @@ class HierarchicalAgglomerativeClustering:
                                '_method1_distance' : self._method1_distance,
                                '_method2_distance' : self._method2_distance,
                                }[linkage]
+
+    @staticmethod
+    def ClearClusters(self):
+        for i in range(len(self.clusters)):
+            for j in range(i, len(self.clusters)):
+                self.clusters[i][j] = 0
+
     
     #метки с минимальным расстоянием, то есть ближайшие кластеры объединяются в новый следующим образом:
     #всем объектам одного кластера присваиваются метки другого, после чего все новые метки,
@@ -49,7 +57,7 @@ class HierarchicalAgglomerativeClustering:
             self.clusters[distance_j, i] = min(distance_i_i, distance_j_j)
             self.clusters[i, distance_i] = min(distance_i_i, distance_j_j)
             self.clusters[i, distance_j] = min(distance_i_i, distance_j_j)
-        self.clusters_points[distance_i].append(self.clusters_points[distance_j])
+        self.clusters_points[distance_i].extend(self.clusters_points[distance_j])
         self.clusters_points.pop(distance_j)
         self.clusters = np.delete(self.clusters, (distance_j), axis=0) # удаление строки
         self.clusters = np.delete(self.clusters, (distance_j), axis=1) # удаление колонки
@@ -57,6 +65,7 @@ class HierarchicalAgglomerativeClustering:
         for clusters_point in self.clusters_points:
             print(clusters_point)
 
+        self.ClearClusters(self)
         print("n = len(clusters) = ", len(self.clusters))
         print("Номера кластеров для точек: ", np.array(self.labels))
         print("Минимальное рассояние между кластерами равно ", distance)
@@ -82,7 +91,7 @@ class HierarchicalAgglomerativeClustering:
             self.clusters[distance_j, i] = max(distance_i_i, distance_j_j)
             self.clusters[i, distance_i] = max(distance_i_i, distance_j_j)
             self.clusters[i, distance_j] = max(distance_i_i, distance_j_j)
-        self.clusters_points[distance_i].append(self.clusters_points[distance_j])
+        self.clusters_points[distance_i].extend(self.clusters_points[distance_j])
         self.clusters_points.pop(distance_j)
         self.clusters = np.delete(self.clusters, (distance_j), axis=0) # удаление строки
         self.clusters = np.delete(self.clusters, (distance_j), axis=1) # удаление колонки
@@ -90,6 +99,7 @@ class HierarchicalAgglomerativeClustering:
         for clusters_point in self.clusters_points:
             print(clusters_point)
 
+        self.ClearClusters(self)
         print("n = len(clusters) = ", len(self.clusters))
         print("Номера кластеров для точек: ", np.array(self.labels))
         print("Максимальное рассояние между кластерами равно ", distance)
@@ -116,7 +126,7 @@ class HierarchicalAgglomerativeClustering:
             self.clusters[distance_j, i] = (distance_i_i * clusters_points_i_len + distance_j_j * clusters_points_j_len) / (clusters_points_i_len + clusters_points_j_len)
             self.clusters[i, distance_i] = (distance_i_i * clusters_points_i_len + distance_j_j * clusters_points_j_len) / (clusters_points_i_len + clusters_points_j_len)
             self.clusters[i, distance_j] = (distance_i_i * clusters_points_i_len + distance_j_j * clusters_points_j_len) / (clusters_points_i_len + clusters_points_j_len)
-        self.clusters_points[distance_i].append(self.clusters_points[distance_j])
+        self.clusters_points[distance_i].extend(self.clusters_points[distance_j])
         self.clusters_points.pop(distance_j)
         self.clusters = np.delete(self.clusters, (distance_j), axis=0) # удаление строки
         self.clusters = np.delete(self.clusters, (distance_j), axis=1) # удаление колонки
@@ -124,6 +134,7 @@ class HierarchicalAgglomerativeClustering:
         for clusters_point in self.clusters_points:
             print(clusters_point)
 
+        self.ClearClusters(self)
         print("n = len(clusters) = ", len(self.clusters))
         print("Номера кластеров для точек: ", np.array(self.labels))
         print("Среднее рассояние между кластерами равно ", distance)
@@ -148,7 +159,7 @@ class HierarchicalAgglomerativeClustering:
             self.clusters[distance_j, i] = (max(distance_i_i, distance_j_j) + min(distance_i_i, distance_j_j)) / 2
             self.clusters[i, distance_i] = (max(distance_i_i, distance_j_j) + min(distance_i_i, distance_j_j)) / 2
             self.clusters[i, distance_j] = (max(distance_i_i, distance_j_j) + min(distance_i_i, distance_j_j)) / 2
-        self.clusters_points[distance_i].append(self.clusters_points[distance_j])
+        self.clusters_points[distance_i].extend(self.clusters_points[distance_j])
         self.clusters_points.pop(distance_j)
         self.clusters = np.delete(self.clusters, (distance_j), axis=0) # удаление строки
         self.clusters = np.delete(self.clusters, (distance_j), axis=1) # удаление колонки
@@ -156,13 +167,14 @@ class HierarchicalAgglomerativeClustering:
         for clusters_point in self.clusters_points:
             print(clusters_point)
 
+        self.ClearClusters(self)
         print("n = len(clusters) = ", len(self.clusters))
         print("Номера кластеров для точек: ", np.array(self.labels))
-        print("Среднее рассояние между кластерами равно ", distance)
+        print("Полусумма минимального и максимального расстояния между объектами из двух кластеров равно ", distance)
         print("И это расстояние между кластерами в D: ", (distance_i, distance_j))
         print('Матрица Dn расстояний между кластерами\n', self.clusters)
     
-    # (сделать) Метод 2. Медианное расстояние между всеми парами объектов из двух кластеров
+    # Метод 2. Медианное расстояние между всеми парами объектов из двух кластеров
     @staticmethod
     def _method2_distance(self):
         distance_i = 1
@@ -173,16 +185,25 @@ class HierarchicalAgglomerativeClustering:
                     distance_i = i
                     distance_j = j
         distance = self.clusters[distance_i, distance_j]
+        point_nums = []
+        for point in self.clusters_points[distance_i]:
+            point_nums.append(point[0])
+        for point in self.clusters_points[distance_j]:
+            point_nums.append(point[0])
+        point_nums.sort()
         for i in range(0, len(self.clusters)):
-            distance_i_i = self.clusters[distance_i][i] if self.clusters[distance_i][i] != 0 else self.clusters[i][distance_i]
-            distance_j_j = self.clusters[distance_j][i] if self.clusters[distance_j][i] != 0 else self.clusters[i][distance_j]
-            clusters_points_i_len = len(self.clusters_points[distance_i])
-            clusters_points_j_len = len(self.clusters_points[distance_j])
-            self.clusters[distance_i, i] = (distance_i_i * clusters_points_i_len + distance_j_j * clusters_points_j_len) / (clusters_points_i_len + clusters_points_j_len)
-            self.clusters[distance_j, i] = (distance_i_i * clusters_points_i_len + distance_j_j * clusters_points_j_len) / (clusters_points_i_len + clusters_points_j_len)
-            self.clusters[i, distance_i] = (distance_i_i * clusters_points_i_len + distance_j_j * clusters_points_j_len) / (clusters_points_i_len + clusters_points_j_len)
-            self.clusters[i, distance_j] = (distance_i_i * clusters_points_i_len + distance_j_j * clusters_points_j_len) / (clusters_points_i_len + clusters_points_j_len)
-        self.clusters_points[distance_i].append(self.clusters_points[distance_j])
+            distances_in_clusters = []
+            for j in range(0, len(self.clusters_copy)):
+                for point_num in point_nums:
+                    distance_in_cluster = self.clusters_copy[point_num][j] if self.clusters_copy[point_num][j] != 0 else self.clusters_copy[j][point_num]
+                    distances_in_clusters.append(distance_in_cluster)
+            self.clusters[distance_i, i] = np.median(distances_in_clusters)
+            self.clusters[distance_j, i] = np.median(distances_in_clusters)
+            self.clusters[i, distance_i] = np.median(distances_in_clusters)
+            self.clusters[i, distance_j] = np.median(distances_in_clusters)
+
+
+        self.clusters_points[distance_i].extend(self.clusters_points[distance_j])
         self.clusters_points.pop(distance_j)
         self.clusters = np.delete(self.clusters, (distance_j), axis=0) # удаление строки
         self.clusters = np.delete(self.clusters, (distance_j), axis=1) # удаление колонки
@@ -190,27 +211,22 @@ class HierarchicalAgglomerativeClustering:
         for clusters_point in self.clusters_points:
             print(clusters_point)
 
+        self.ClearClusters(self)
         print("n = len(clusters) = ", len(self.clusters))
         print("Номера кластеров для точек: ", np.array(self.labels))
-        print("Среднее рассояние между кластерами равно ", distance)
+        print("Медианное расстояние между всеми парами объектов из двух кластеров равно ", distance)
         print("И это расстояние между кластерами в D: ", (distance_i, distance_j))
         print('Матрица Dn расстояний между кластерами\n', self.clusters)
-
-
-        # distances = []
-        # for i in c1:
-        #     for j in c2:
-        #         distances.append(sqeuclidean(i, j))
-        # print(distances)
-        # return np.median(distances)
     
     # функция предсказания
     def fit_predict(self, points):
         self.clusters = ManhattanDistance(points)
+        self.clusters_copy = copy.copy(self.clusters)
         print('Манхэттенское расстояние\n', self.clusters)
         self.labels = np.arange(len(points))
         
-        self.clusters_points = [[point] for point in points]
+        self.clusters_points = [point for point in points]
+        self.clusters_points = [[[point, self.clusters_points[point]]] for point in range(len(points))]
         step = 0
         while len(self.clusters) > self.n_clusters:
             print("\nШаг ", step)
@@ -223,6 +239,6 @@ class HierarchicalAgglomerativeClustering:
 points = np.array([[2, 2, 4, 4, 5, 5, 7, 7],
              [1, 5, 3, 6, 4, 5, 2, 5]]).T
 print(points)
-ac = HierarchicalAgglomerativeClustering(n_clusters=2, linkage='_method2_distance')
+ac = HierarchicalAgglomerativeClustering(n_clusters=2, linkage='_method1_distance')
 ac_pred_res = ac.fit_predict(points)
 print('\nРезультат предсказания', ac_pred_res)
